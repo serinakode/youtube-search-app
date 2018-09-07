064cb3c51c6ee0834df6df17f0313d770045eaff
Serina Ko <serinako08@gmail.com>
2018-09-07 14:22:19 +1000

    execute youtube search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
 
 const API_KEY = 'AIzaSyD_eDueAYrfhBiGiLYivWoKQ75AduE0QKI';
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
